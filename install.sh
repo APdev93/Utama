4c9405b4f633f0e31eff8477d9a8acc2e183b7a1
Arr_Dev <83260062+ArrDev10@users.noreply.github.com>
2023-07-20 00:04:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r Utama
-    git clone https://github.com/ardian458/Utama.git
+    git clone https://github.com/ArrDev10/Utama.git
     cd Utama
     rm /var/www/pterodactyl/resources/scripts/Utama.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
